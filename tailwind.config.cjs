8f982c7870ab25693a65fb8e07404d40f27dd1e1
Edwin Ramos <edwin_ramos2@usmp.pe>
2025-04-21 21:58:46 -0500

    [FIX] Modificación de dimensiones de las secciones

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: ['./src/**/*.{astro,html,css,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
       extend: {
-        colors: {
-          customGreen: 'rgb(8, 146, 100)',
-        },
+        colors: {},
       },
     },
     plugins: [],
